5ddcad6c7eac1af7889a69db04a563390f73408d
Luis Kreutzer <luisfkreutzer@icloud.com>
2024-10-02 11:50:16 +0200

    Added Opertors for parsing

---
+++ src/utils/operators.py
@@ -0,0 +1,15 @@
+BIN_OP_OPERATORS = [
+    "TT_plus", 
+    "TT_sub", 
+    "TT_mul",
+    "TT_div"
+]
+EXPR_OPERATORS = [
+    "TT_plus", 
+    "TT_sub",
+    "TT_mul", 
+    "TT_div", 
+    "TT_equ", 
+    "TT_greater", 
+    "TT_less",
+]
